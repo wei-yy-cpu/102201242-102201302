d8a09d4fad9469a6329eb9adacdb6eca575903e1
梦游大观园 <3185903988@qq.com>
2024-10-10 22:06:19 +0800

    Update admin_model.js

--- cloudfunctions/score_cloud/model/admin_model.js
+++ cloudfunctions/score_cloud/model/admin_model.js
@@ -1,19 +1,3 @@
-// +----------------------------------------------------------------------
-// | CCMiniCloud [ Cloud Framework ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2021 www.code942.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 明章科技
-// +----------------------------------------------------------------------
- 
-/**
- * Notes: 系统管理员实体
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-10-16 19:20:00
- * Version : CCMiniCloud Framework Ver 2.0.1 ALL RIGHTS RESERVED BY 明章科技
- */
 
 
 const BaseModel = require('./base_ccmini_model.js');
