7b048e0ceaed9cd7d4b2454fcf854ae9c014c153
梦游大观园 <3185903988@qq.com>
2024-10-10 22:10:04 +0800

    Update ccmini_biz_helper.js

--- miniprogram/score/helper/ccmini_biz_helper.js
+++ miniprogram/score/helper/ccmini_biz_helper.js
@@ -1,8 +1,4 @@
-/**
- * Notes: 业务通用
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-11-14 07:48:00 
- */
+
 
 const ccminiCacheHelper = require('./ccmini_cache_helper.js');
 const CCMINI_SETTING = require('./ccmini_setting.js');
