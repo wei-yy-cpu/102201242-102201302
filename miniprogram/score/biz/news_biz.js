89ddbb871edad72033d862dba10cf3793433b924
梦游大观园 <3185903988@qq.com>
2024-10-10 22:07:41 +0800

    Update news_biz.js

--- miniprogram/score/biz/news_biz.js
+++ miniprogram/score/biz/news_biz.js
@@ -1,8 +1,4 @@
-/**
- * Notes: 公告通知模块业务逻辑
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-11-14 07:48:00 
- */
+
 
 const BaseCCMiniBiz = require('./base_ccmini_biz.js'); 
 
