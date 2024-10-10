79ef0a8fed16adafc8bbee36a6fc12dd90ef768d
梦游大观园 <3185903988@qq.com>
2024-10-10 22:08:23 +0800

    Update user_biz.js

--- miniprogram/score/biz/user_biz.js
+++ miniprogram/score/biz/user_biz.js
@@ -1,8 +1,3 @@
-/**
- * Notes: 用户模块业务逻辑
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-11-14 07:48:00 
- */
 
 const BaseCCMiniBiz = require('./base_ccmini_biz.js');
 const ccminiCloudHelper = require('../helper/ccmini_cloud_helper.js');
