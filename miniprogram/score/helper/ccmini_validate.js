f89599a259c68b70ce6a9352816eca85fa5f0115
梦游大观园 <3185903988@qq.com>
2024-10-10 22:11:31 +0800

    Update ccmini_validate.js

--- miniprogram/score/helper/ccmini_validate.js
+++ miniprogram/score/helper/ccmini_validate.js
@@ -1,8 +1,3 @@
- /**
-  * Notes: 数据校验类库
-  * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
-  * Date: 2020-11-14 07:48:00 
-  */
 
  const ccminiHelper = require("./ccmini_helper.js");
 
