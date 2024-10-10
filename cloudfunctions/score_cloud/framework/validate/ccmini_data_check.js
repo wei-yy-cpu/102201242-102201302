7772c26c4c7ee8cd8b31c2eae86b9b9a2924653e
梦游大观园 <3185903988@qq.com>
2024-10-10 22:06:06 +0800

    Update ccmini_data_check.js

--- cloudfunctions/score_cloud/framework/validate/ccmini_data_check.js
+++ cloudfunctions/score_cloud/framework/validate/ccmini_data_check.js
@@ -1,19 +1,3 @@
-// +----------------------------------------------------------------------
-// | CCMiniCloud [ Cloud Framework ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2021 www.code942.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 明章科技
-// +----------------------------------------------------------------------
-
-/**
- * Notes: 数据校验
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-09-05 04:00:00
- * Version : CCMiniCloud Framework Ver 2.0.1 ALL RIGHTS RESERVED BY 明章科技
- */
 
 const ccminiUtil = require('../utils/ccmini_util.js');
 const CCMiniAppError = require('../handler/ccmini_app_error.js');
